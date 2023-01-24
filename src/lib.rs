85be8a3051b471b36d55ab670f23afc40dfc94c0
Reilly Wood <reilly.wood@icloud.com>
2023-01-23 19:00:09 -0800

    Remove sys-info dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,8 @@
 extern crate is_docker;
 extern crate once_cell;
-extern crate sys_info;
 
 use once_cell::sync::OnceCell;
-
-fn proc_version_includes_microsoft() -> bool {
-    match std::fs::read_to_string("/proc/version") {
-        Ok(file_contents) => file_contents.to_lowercase().contains("microsoft"),
-        Err(_) => false,
-    }
-}
+use std::{fs::File, io::Read};
 
 pub fn is_wsl() -> bool {
     static CACHED_RESULT: OnceCell<bool> = OnceCell::new();
@@ -19,17 +12,10 @@ pub fn is_wsl() -> bool {
             return false;
         }
 
-        if sys_info::os_release().is_ok()
-            && sys_info::os_release()
-                .unwrap()
-                .to_lowercase()
-                .contains("microsoft")
-        {
-            if is_docker::is_docker() {
-                return false;
+        if let Ok(os_release) = get_os_release() {
+            if os_release.to_lowercase().contains("microsoft") {
+                return !is_docker::is_docker();
             }
-
-            return true;
         }
 
         if proc_version_includes_microsoft() {
@@ -38,4 +24,42 @@ pub fn is_wsl() -> bool {
             false
         }
     })
+}
+
+fn proc_version_includes_microsoft() -> bool {
+    match std::fs::read_to_string("/proc/version") {
+        Ok(file_contents) => file_contents.to_lowercase().contains("microsoft"),
+        Err(_) => false,
+    }
+}
+
+// This function is copied from the sys-info crate to avoid taking a dependency on all of sys-info
+// https://docs.rs/sys-info/0.9.1/src/sys_info/lib.rs.html#426-433
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Siyu Wang
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+fn get_os_release() -> Result<String, std::io::Error> {
+    let mut s = String::new();
+    File::open("/proc/sys/kernel/osrelease")?.read_to_string(&mut s)?;
+    s.pop(); // pop '\n'
+    Ok(s)
 }
